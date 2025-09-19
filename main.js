04cb6096962351b7d176ecf61b3b1546a5f72162
Aidan <aidan.yun@outlook.com>
2025-09-18 22:26:32 -0700

    updated GUI and Image function

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const myInput = document.getElementById('userInput');
 
-function getPokemon() {
+async function getPokemon() {
     const inputValue = myInput.value.trim();
 
     if (!inputValue) {
@@ -16,10 +16,59 @@ function getPokemon() {
     }
 
     try {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}/`)
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
+
+        const pokemonName = document.getElementById("pokemonName");
+        
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}/`)
+ 
+            if (!response.ok) {
+                throw new Error("Resource could not be fetched");
+            }
+            
+            const data = await response.json();
+            console.log(data);      // data will be shown in console
+            
+            // for image
+
+            const pokemonSprite = data.sprites.front_default;
+            const imageElement = document.getElementById("pokemonSprite");
+            const pokeball = document.getElementById("pokeball");
+            
+            if (pokemonSprite) {
+                pokeball.style.display = "none";
+                imageElement.src = pokemonSprite;
+                imageElement.style.display = "block";
+            } 
+            else {
+                imageElement.style.display = "none";
+                pokeball.style.display = "block";
+            }
+
+            // for name
+            const name = document.getElementById("name");
+            name.textContent = `Name: ${data.name}`;
+
+            // for weight
+            const weight = document.getElementById("weight");
+            const newWeight = (data.weight * 100) * 0.0022;
+            weight.textContent = `Weight: ${newWeight.toFixed(2)} lbs`;
+
+            // for height
+            const height = document.getElementById("height");
+            const heightInMeters = data.height / 10;
+            const heightInFeet = heightInMeters * 3.28084;      // total feet
+
+            const feet = Math.floor(heightInFeet);
+            const inches = Math.round((heightInFeet - feet) * 12);      // feet to inches conversion
+            height.textContent = `Height: ${feet}'${inches}''`;
+
+            const type = document.getElementById("type");
+            const typeNames = data.types.map(typeInfo => {
+                const typeName = typeInfo.type.name;
+                return typeName.charAt(0).toUpperCase() + typeName.slice(1);
+            }).join(', ');
+            type.textContent = `Type: ${typeNames}`;
+
     }
     catch(error) {
         console.error(error);
